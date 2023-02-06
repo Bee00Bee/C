5c0eda50f9cca03ca58fe22fae9540cb62fd0e20
Bee00Bee <96251369+Bee00Bee@users.noreply.github.com>
2023-02-06 21:33:14 +0000

    declaration.c added

--- declaration.c
+++ declaration.c
@@ -4,7 +4,7 @@ int main(void){
     int a=5, b=7, c=6;
     double average=0.0;
     printf("a= %d, b= %d, c= %d", a, b, c);
-    average = (a+b+c)/3;
+    average = (a+b+c)/3.0;
     printf("\n\naverage= %lf\n\n", average);
     return (0);
 }
